f5f006e5e48932e4d21c044a1ea92e321142aaad
Russ Cox <rsc@swtch.com>
2010-03-10 18:12:59 -0800

    port to FreeBSD; probably NetBSD, OpenBSD too

--- re2/dfa.cc
+++ re2/dfa.cc
@@ -33,8 +33,8 @@ DEFINE_bool(re2_dfa_bail_when_slow, true,
 
 namespace re2 {
 
-#ifdef __APPLE__
-// Mac OS X lacks memrchr()
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+// BSD systems lack memrchr.
 static void* memrchr(const void* s, int c, size_t n) {
   const unsigned char* p = (const unsigned char*)s;
   for (p += n; n > 0; n--)
