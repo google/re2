fc5be8a51be1cd71d926b4afc8487aee53c1605a
Russ Cox <rsc@swtch.com>
2012-10-21 10:47:29 -0400

    re2: fix type mismatch in DFA::Search
    R=rsc, rsc
    CC=re2-dev
    http://codereview.appspot.com/6738058

--- re2/dfa.cc
+++ re2/dfa.cc
@@ -1778,7 +1778,7 @@ bool DFA::Search(const StringPiece& text,
     return false;
   }
   if (params.start == DeadState)
-    return NULL;
+    return false;
   if (params.start == FullMatchState) {
     if (run_forward == want_earliest_match)
       *epp = text.begin();
