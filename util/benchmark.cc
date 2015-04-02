230ff1358f843f1539ef1b7783d81a6373fce571
Han-Wen Nienhuys <hanwen@google.com>
2015-04-02 17:02:44 +0200

    Fix missing system header include.
    Fixes #16.
    
    Change-Id: Iaf2dd1da12f9c64a8b3c2ca63881c67e1d2b5d1c

--- util/benchmark.cc
+++ util/benchmark.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+#include <sys/time.h>
+
 #include "util/util.h"
 #include "util/flags.h"
 #include "util/benchmark.h"
