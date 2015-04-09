ebf483d38629408f4375959c6ec040bc7f07d25c
Thiago Farina <tfarina@chromium.org>
2015-04-09 00:01:35 -0300

    Cleanup: Update link to gflags project site.
    It isn't hosted in code.google.com anymore. Now it lives in Github.
    
    Change-Id: I6017bf476bff7eec4d00856bce115859aa2a7672
    Reviewed-on: https://code-review.googlesource.com/2220
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- util/flags.h
+++ util/flags.h
@@ -5,7 +5,7 @@
 // Simplified version of Google's command line flags.
 // Does not support parsing the command line.
 // If you want to do that, see
-// http://code.google.com/p/google-gflags
+// https://gflags.github.io/gflags/
 
 #ifndef RE2_UTIL_FLAGS_H__
 #define RE2_UTIL_FLAGS_H__
