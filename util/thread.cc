3faf14ff6c880b9d4173b8dfd2c0a8ab9eef10f1
Paul Wankadia <junyer@google.com>
2015-09-29 14:10:00 +1000

    Tidy up threading handling of Windows versus non-Windows.
    Fixes #53.
    
    Change-Id: I102273a99618a4619a1e726f3c76c3880be869bc

--- util/thread.cc
+++ util/thread.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#include <pthread.h>
-
 #include "util/util.h"
 #include "util/thread.h"
 
