bc5a007f57eb1ac6bb307f995c91e9bcab683f88
Paul Wankadia <junyer@google.com>
2015-09-29 14:10:00 +1000

    Tidy up threading handling of Windows versus non-Windows.
    Fixes #53.
    
    Change-Id: I102273a99618a4619a1e726f3c76c3880be869bc
    Reviewed-on: https://code-review.googlesource.com/3499
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- util/thread.h
+++ util/thread.h
@@ -5,7 +5,11 @@
 #ifndef RE2_UTIL_THREAD_H__
 #define RE2_UTIL_THREAD_H__
 
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <pthread.h>
+#endif
 
 class Thread {
  public:
@@ -15,12 +19,15 @@ class Thread {
   void Join();
   void SetJoinable(bool);
   virtual void Run() = 0;
- 
+
  private:
+#ifdef _WIN32
+  HANDLE pid_;
+#else
   pthread_t pid_;
+#endif
   bool running_;
   bool joinable_;
 };
 
 #endif  // RE2_UTIL_THREAD_H__
-
