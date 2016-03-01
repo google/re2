2d019355e996968eb8088c6c33b37294b437dada
Paul Wankadia <junyer@google.com>
2016-03-01 21:11:18 +1100

    Remove unused LinkerInitialized enum.
    Change-Id: Iab3bdcdb612de8d702b53003cfc19ad331dc07fe

--- re2/re2.h
+++ re2/re2.h
@@ -197,21 +197,6 @@ class Mutex;
 class Prog;
 class Regexp;
 
-// The following enum should be used only as a constructor argument to indicate
-// that the variable has static storage class, and that the constructor should
-// do nothing to its state.  It indicates to the reader that it is legal to
-// declare a static instance of the class, provided the constructor is given
-// the LINKER_INITIALIZED argument.  Normally, it is unsafe to declare a
-// static variable that has a constructor or a destructor because invocation
-// order is undefined.  However, IF the type can be initialized by filling with
-// zeroes (which the loader does for static variables), AND the type's
-// destructor does nothing to the storage, then a constructor for static
-// initialization can be declared as
-//       explicit MyClass(LinkerInitialized x) {}
-// and invoked as
-//       static MyClass my_variable_name(LINKER_INITIALIZED);
-enum LinkerInitialized { LINKER_INITIALIZED };
-
 // Interface for regular expression matching.  Also corresponds to a
 // pre-compiled regular expression.  An "RE2" object is safe for
 // concurrent use by multiple threads.
