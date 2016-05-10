a476a9a4e66da2083f1f7182591a28b66d2f7cbb
Paul Wankadia <junyer@google.com>
2016-05-10 16:29:17 +1000

    Add a test for equivalence classes in Latin-1.
    Change-Id: I4093f530216a4a6cb93863a347638b9092719708
    Reviewed-on: https://code-review.googlesource.com/4720
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- re2/testing/compile_test.cc
+++ re2/testing/compile_test.cc
@@ -143,30 +143,55 @@ TEST(TestRegexpCompileToProg, Simple) {
   EXPECT_EQ(failed, 0);
 }
 
+// The distinct byte ranges involved in the Latin-1 dot ([^\n]).
+static struct Latin1ByteRange {
+  int lo;
+  int hi;
+  int ec;  // equivalence class
+} latin1ranges[] = {
+  { 0x00, 0x09, 0 },
+  { 0x0A, 0x0A, 1 },
+  { 0x0B, 0xFF, 2 },
+};
+
+TEST(TestCompile, Latin1Ranges) {
+  Regexp* re = Regexp::Parse(".", Regexp::PerlX|Regexp::Latin1, NULL);
+  EXPECT_TRUE(re != NULL);
+  Prog* prog = re->CompileToProg(0);
+  EXPECT_TRUE(prog != NULL);
+  EXPECT_EQ(prog->bytemap_range(), arraysize(latin1ranges));
+  for (int i = 0; i < arraysize(latin1ranges); i++)
+    for (int j = latin1ranges[i].lo; j <= latin1ranges[i].hi; j++)
+      EXPECT_EQ(prog->bytemap()[j], latin1ranges[i].ec) << " byte " << j;
+  delete prog;
+  re->Decref();
+}
+
 // The distinct byte ranges involved in the UTF-8 dot ([^\n]).
 // Once, erroneously split between 0x3f and 0x40 because it is
 // a 6-bit boundary.
 static struct UTF8ByteRange {
   int lo;
   int hi;
+  int ec;  // equivalence class
 } utf8ranges[] = {
-  { 0x00, 0x09 },
-  { 0x0A, 0x0A },
-  { 0x0B, 0x7F },
-  { 0x80, 0x8F },
-  { 0x90, 0x9F },
-  { 0xA0, 0xBF },
-  { 0xC0, 0xC1 },
-  { 0xC2, 0xDF },
-  { 0xE0, 0xE0 },
-  { 0xE1, 0xEF },
-  { 0xF0, 0xF0 },
-  { 0xF1, 0xF3 },
-  { 0xF4, 0xF4 },
-  { 0xF5, 0xFF },
+  { 0x00, 0x09, 0 },
+  { 0x0A, 0x0A, 1 },
+  { 0x0B, 0x7F, 2 },
+  { 0x80, 0x8F, 3 },
+  { 0x90, 0x9F, 4 },
+  { 0xA0, 0xBF, 5 },
+  { 0xC0, 0xC1, 6 },
+  { 0xC2, 0xDF, 7 },
+  { 0xE0, 0xE0, 8 },
+  { 0xE1, 0xEF, 9 },
+  { 0xF0, 0xF0, 10 },
+  { 0xF1, 0xF3, 11 },
+  { 0xF4, 0xF4, 12 },
+  { 0xF5, 0xFF, 13 },
 };
 
-TEST(TestCompile, ByteRanges) {
+TEST(TestCompile, UTF8Ranges) {
   Regexp* re = Regexp::Parse(".", Regexp::PerlX, NULL);
   EXPECT_TRUE(re != NULL);
   Prog* prog = re->CompileToProg(0);
@@ -174,7 +199,7 @@ TEST(TestCompile, ByteRanges) {
   EXPECT_EQ(prog->bytemap_range(), arraysize(utf8ranges));
   for (int i = 0; i < arraysize(utf8ranges); i++)
     for (int j = utf8ranges[i].lo; j <= utf8ranges[i].hi; j++)
-      EXPECT_EQ(prog->bytemap()[j], i) << " byte " << j;
+      EXPECT_EQ(prog->bytemap()[j], utf8ranges[i].ec) << " byte " << j;
   delete prog;
   re->Decref();
 }
