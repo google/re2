6bf3f71b1f65bc858f05da0c94d0b93a76e7f319
Paul Wankadia <junyer@google.com>
2016-08-02 22:19:07 +1000

    Fix testinstall.cc.
    Change-Id: Id1760addd0b55ccb6d6bb9c2c8ef10a874f9bcd3
    Reviewed-on: https://code-review.googlesource.com/5444
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- testinstall.cc
+++ testinstall.cc
@@ -12,9 +12,9 @@ int main(void) {
 	FilteredRE2 f;
 	int id;
 	f.Add("a.*b.*c", RE2::DefaultOptions, &id);
-	vector<string> v;
+	std::vector<string> v;
 	f.Compile(&v);
-	vector<int> ids;
+	std::vector<int> ids;
 	f.FirstMatch("abbccc", ids);
 
 	if(RE2::FullMatch("axbyc", "a.*b.*c")) {
