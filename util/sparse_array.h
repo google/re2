6a6df532661ba68481acad3146839c72977a9f6e
Paul Wankadia <junyer@google.com>
2016-08-29 01:46:05 +1000

    Undo the nerfing of SparseArray<> and SparseSet.
    The undefined behaviour still needs to be fixed, but
    the performance degradation is too much at this point.
    
    Change-Id: I5a9dfe6cf9de96dade373b70ffb411f0ba28e861
    Reviewed-on: https://code-review.googlesource.com/7152
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- util/sparse_array.h
+++ util/sparse_array.h
@@ -15,7 +15,8 @@
 //
 // Insertion and deletion are constant time operations.
 //
-// Allocating the array is an O(n) operation.
+// Allocating the array is a constant time operation
+// when memory allocation is a constant time operation.
 //
 // Clearing the array is a constant time operation (unusual!).
 //
@@ -375,7 +376,7 @@ class SparseArray<Value>::IndexValue {
   typedef int first_type;
   typedef Value second_type;
 
-  IndexValue() : index_() {}
+  IndexValue() {}
   IndexValue(int i, const Value& v) : index_(i), second(v) {}
   IndexValue(int i, Value&& v) : index_(i), second(std::move(v)) {}
 
@@ -411,11 +412,19 @@ void SparseArray<Value>::resize(int max_size) {
   DebugCheckInvariants();
   if (max_size > max_size_) {
     std::unique_ptr<int[]> a(new int[max_size]);
-    std::copy_n(sparse_to_dense_.get(), max_size_, a.get());
-    std::fill(a.get() + max_size_, a.get() + max_size, 0);
+    if (sparse_to_dense_) {
+      std::copy_n(sparse_to_dense_.get(), max_size_, a.get());
+    }
     sparse_to_dense_ = std::move(a);
 
     dense_.resize(max_size);
+
+#ifdef MEMORY_SANITIZER
+    for (int i = max_size_; i < max_size; i++) {
+      sparse_to_dense_[i] = 0xababababU;
+      dense_[i].index_ = 0xababababU;
+    }
+#endif
   }
   max_size_ = max_size;
   if (size_ > max_size_)
@@ -474,9 +483,17 @@ void SparseArray<Value>::create_index(int i) {
 
 template<typename Value> SparseArray<Value>::SparseArray(int max_size) {
   max_size_ = max_size;
-  sparse_to_dense_ = std::unique_ptr<int[]>(new int[max_size]());
+  sparse_to_dense_ = std::unique_ptr<int[]>(new int[max_size]);
   dense_.resize(max_size);
   size_ = 0;
+
+#ifdef MEMORY_SANITIZER
+  for (int i = 0; i < max_size; i++) {
+    sparse_to_dense_[i] = 0xababababU;
+    dense_[i].index_ = 0xababababU;
+  }
+#endif
+
   DebugCheckInvariants();
 }
 
