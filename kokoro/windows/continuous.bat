a3d5d4b6cc6ddad03bfd6c18ebd546ac6470d3b2
Paul Wankadia <junyer@google.com>
2016-08-30 22:08:59 +1000

    Tweak the CI script for CMake on Windows.
    Change-Id: I10f78c381c3aaf542952863ac6d6e9398d2fbb65

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -1,11 +1,11 @@
-CD git/re2                              || EXIT /B 1
+CD git/re2                                                          || EXIT /B 1
 
-cmake .                                 || EXIT /B 1
-cmake --build . --clean-first           || EXIT /B 1
-ctest -E dfa^|exhaustive^|random        || EXIT /B 1
+cmake -D CMAKE_BUILD_TYPE=Debug -A x64 .                            || EXIT /B 1
+cmake --build . --config Debug --clean-first                        || EXIT /B 1
+ctest --build-config Debug -E dfa^|exhaustive^|random               || EXIT /B 1
 
-cmake -DCMAKE_BUILD_TYPE=Release .      || EXIT /B 1
-cmake --build . --clean-first           || EXIT /B 1
-ctest -E dfa^|exhaustive^|random        || EXIT /B 1
+cmake -D CMAKE_BUILD_TYPE=Release -A x64 .                          || EXIT /B 1
+cmake --build . --config Release --clean-first                      || EXIT /B 1
+ctest --build-config Release -E dfa^|exhaustive^|random             || EXIT /B 1
 
 EXIT /B 0
