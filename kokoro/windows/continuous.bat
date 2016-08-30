049f4dd86d67db911713d8a75fa6a2ec17d4c903
Paul Wankadia <junyer@google.com>
2016-08-30 22:24:16 +1000

    Get test output on failure from CTest.
    Change-Id: I3cec348d1bcd3179b6b89618b89b4be80a9d0e57

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -2,10 +2,10 @@ CD git/re2                                                          || EXIT /B 1
 
 cmake -D CMAKE_BUILD_TYPE=Debug -A x64 .                            || EXIT /B 1
 cmake --build . --config Debug --clean-first                        || EXIT /B 1
-ctest --build-config Debug -E dfa^|exhaustive^|random               || EXIT /B 1
+ctest -C Debug -E dfa^|exhaustive^|random --output-on-failure       || EXIT /B 1
 
 cmake -D CMAKE_BUILD_TYPE=Release -A x64 .                          || EXIT /B 1
 cmake --build . --config Release --clean-first                      || EXIT /B 1
-ctest --build-config Release -E dfa^|exhaustive^|random             || EXIT /B 1
+ctest -C Release -E dfa^|exhaustive^|random --output-on-failure     || EXIT /B 1
 
 EXIT /B 0
