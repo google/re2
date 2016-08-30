c5094dd2ab958d597120f382a5a25de794776b5b
Paul Wankadia <junyer@google.com>
2016-08-30 23:07:06 +1000

    Get test output on failure from Bazel.
    Change-Id: I963276be68d5521f933a2f4df07b020fd2d9b1cf
    Reviewed-on: https://code-review.googlesource.com/7222
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- kokoro/ubuntu/continuous.sh
+++ kokoro/ubuntu/continuous.sh
@@ -5,7 +5,7 @@ cd git/re2
 
 bazel clean
 bazel build --compilation_mode=dbg -- //...
-bazel test  --compilation_mode=dbg -- //... \
+bazel test  --compilation_mode=dbg --test_output=errors -- //... \
   -//:dfa_test \
   -//:exhaustive1_test \
   -//:exhaustive2_test \
@@ -15,7 +15,7 @@ bazel test  --compilation_mode=dbg -- //... \
 
 bazel clean
 bazel build --compilation_mode=opt -- //...
-bazel test  --compilation_mode=opt -- //... \
+bazel test  --compilation_mode=opt --test_output=errors -- //... \
   -//:dfa_test \
   -//:exhaustive1_test \
   -//:exhaustive2_test \
