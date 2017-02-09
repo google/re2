d1715575ad9aafb116355f4233f7698f483bc081
Paul Wankadia <junyer@google.com>
2017-02-09 15:05:04 +1100

    Make the RE2 fuzzer ignore inputs larger than 1KiB.
    The hope is to find a "sweet spot" for fuzzing: high coverage and high
    speed. At the moment, we waste too much time on very large inputs that
    give us very little.
    
    Change-Id: I94ee93f1d5293c63dc5fd749f47087cdc7acd43f
    Reviewed-on: https://code-review.googlesource.com/10790
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- re2/fuzzing/re2_fuzzer.cc
+++ re2/fuzzing/re2_fuzzer.cc
@@ -50,7 +50,7 @@ void Test(StringPiece pattern, const RE2::Options& options, StringPiece text) {
 
 // Entry point for libFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  if (size == 0 || size > 1000000)
+  if (size == 0 || size > 1024)
     return 0;
 
   // The one-at-a-time hash by Bob Jenkins.
