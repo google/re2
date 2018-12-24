2cf86e5ab6dcfe045a1f510c2b9a8b012a4158cd
Paul Wankadia <junyer@google.com>
2018-12-24 05:13:27 -0800

    Compute num_captures_ eagerly.
    Change-Id: I7f77eece4c553956e61c746a77c72e095fb1d0e5
    Reviewed-on: https://code-review.googlesource.com/c/36874
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- re2/re2.cc
+++ re2/re2.cc
@@ -177,10 +177,10 @@ void RE2::Init(const StringPiece& pattern, const Options& options) {
   entire_regexp_ = NULL;
   suffix_regexp_ = NULL;
   prog_ = NULL;
+  num_captures_ = -1;
   rprog_ = NULL;
   error_ = empty_string;
   error_code_ = NoError;
-  num_captures_ = -1;
   named_groups_ = NULL;
   group_names_ = NULL;
 
@@ -217,6 +217,11 @@ void RE2::Init(const StringPiece& pattern, const Options& options) {
     error_code_ = RE2::ErrorPatternTooLarge;
     return;
   }
+
+  // We used to compute this lazily, but it's used during the
+  // typical control flow for a match call, so we now compute
+  // it eagerly, which avoids the overhead of std::once_flag.
+  num_captures_ = suffix_regexp_->NumCaptures();
 
   // Could delay this until the first match call that
   // cares about submatch information, but the one-pass
@@ -299,16 +304,6 @@ int RE2::ReverseProgramFanout(std::map<int, int>* histogram) const {
   if (prog == NULL)
     return -1;
   return Fanout(prog, histogram);
-}
-
-// Returns num_captures_, computing it if needed, or -1 if the
-// regexp wasn't valid on construction.
-int RE2::NumberOfCapturingGroups() const {
-  std::call_once(num_captures_once_, [](const RE2* re) {
-    if (re->suffix_regexp_ != NULL)
-      re->num_captures_ = re->suffix_regexp_->NumCaptures();
-  }, this);
-  return num_captures_;
 }
 
 // Returns named_groups_, computing it if needed.
