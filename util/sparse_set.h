d4cec3fa7535c897d2ad9cab29a86e59aacb1291
Chris Kennelly <ckennelly@google.com>
2019-01-09 17:08:15 -0500

    Ensure we succeed at constructing new sparse and dense arrays.
    We could conceivably fail (in the presence of exceptions) to construct
    the new dense array after constructing and replacing the sparse array.
    
    Change-Id: I0fa0153c58e8b7de66aee464519a378b7f8c0bc8
    Reviewed-on: https://code-review.googlesource.com/c/37291
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit b695a2d771b8df8e4205d09b1b4647d2561db783)

--- util/sparse_set.h
+++ util/sparse_set.h
@@ -200,13 +200,12 @@ void SparseSetT<Value>::resize(int max_size) {
       std::copy_n(sparse_.data(), old_max_size, a.data());
     }
 
-    sparse_ = std::move(a);
-
     PODArray<int> b(max_size);
     if (dense_.data() != NULL) {
       std::copy_n(dense_.data(), old_max_size, b.data());
     }
 
+    sparse_ = std::move(a);
     dense_ = std::move(b);
 
     MaybeInitializeMemory(old_max_size, max_size);
