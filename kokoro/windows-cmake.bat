4b787623c216c444b2ea295183ded7a100e1b593
Paul Wankadia <junyer@google.com>
2019-01-31 08:43:31 -0800

    Use bash on Windows since Kokoro offers it.
    Change-Id: Ied4073f8de5d2056da16b040612f239d2f05c9ce
    Reviewed-on: https://code-review.googlesource.com/c/37952
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- kokoro/windows-cmake.bat
+++ kokoro/windows-cmake.bat
@@ -1,18 +1,2 @@
-CD git/re2 ^
-  || EXIT /B 1
-
-cmake -D CMAKE_BUILD_TYPE=Debug -G "Visual Studio 14 2015" -A x64 . ^
-  || EXIT /B 1
-cmake --build . --config Debug --clean-first ^
-  || EXIT /B 1
-ctest -C Debug --output-on-failure -E dfa^|exhaustive^|random ^
-  || EXIT /B 1
-
-cmake -D CMAKE_BUILD_TYPE=Release -G "Visual Studio 14 2015" -A x64 . ^
-  || EXIT /B 1
-cmake --build . --config Release --clean-first ^
-  || EXIT /B 1
-ctest -C Release --output-on-failure -E dfa^|exhaustive^|random ^
-  || EXIT /B 1
-
-EXIT /B 0
+bash git/re2/kokoro/windows-cmake.sh
+EXIT /B %ERRORLEVEL%
