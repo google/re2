5ccbedf6c168e607fdda3e4c68fb6609088f4da9
Paul Wankadia <junyer@google.com>
2019-03-08 04:10:47 -0800

    Compute hints for ByteRange instructions. Make BitState use them.
    Change-Id: Id7d6c1470c7e2b7dc7e13762577764adb21c8454
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/38790
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit a7ff8e76292e458c947999968a4837e2bcd7aae2)

--- re2/testing/compile_test.cc
+++ re2/testing/compile_test.cc
@@ -26,88 +26,88 @@ struct Test {
 
 static Test tests[] = {
   { "a",
-    "3. byte [61-61] -> 4\n"
+    "3. byte [61-61] 0 -> 4\n"
     "4. match! 0\n" },
   { "ab",
-    "3. byte [61-61] -> 4\n"
-    "4. byte [62-62] -> 5\n"
+    "3. byte [61-61] 0 -> 4\n"
+    "4. byte [62-62] 0 -> 5\n"
     "5. match! 0\n" },
   { "a|c",
-    "3+ byte [61-61] -> 5\n"
-    "4. byte [63-63] -> 5\n"
+    "3+ byte [61-61] 0 -> 5\n"
+    "4. byte [63-63] 0 -> 5\n"
     "5. match! 0\n" },
   { "a|b",
-    "3. byte [61-62] -> 4\n"
+    "3. byte [61-62] 0 -> 4\n"
     "4. match! 0\n" },
   { "[ab]",
-    "3. byte [61-62] -> 4\n"
+    "3. byte [61-62] 0 -> 4\n"
     "4. match! 0\n" },
   { "a+",
-    "3. byte [61-61] -> 4\n"
+    "3. byte [61-61] 0 -> 4\n"
     "4+ nop -> 3\n"
     "5. match! 0\n" },
   { "a+?",
-    "3. byte [61-61] -> 4\n"
+    "3. byte [61-61] 0 -> 4\n"
     "4+ match! 0\n"
     "5. nop -> 3\n" },
   { "a*",
-    "3+ byte [61-61] -> 3\n"
+    "3+ byte [61-61] 1 -> 3\n"
     "4. match! 0\n" },
   { "a*?",
     "3+ match! 0\n"
-    "4. byte [61-61] -> 3\n" },
+    "4. byte [61-61] 0 -> 3\n" },
   { "a?",
-    "3+ byte [61-61] -> 5\n"
+    "3+ byte [61-61] 1 -> 5\n"
     "4. nop -> 5\n"
     "5. match! 0\n" },
   { "a??",
     "3+ nop -> 5\n"
-    "4. byte [61-61] -> 5\n"
+    "4. byte [61-61] 0 -> 5\n"
     "5. match! 0\n" },
   { "a{4}",
-    "3. byte [61-61] -> 4\n"
-    "4. byte [61-61] -> 5\n"
-    "5. byte [61-61] -> 6\n"
-    "6. byte [61-61] -> 7\n"
+    "3. byte [61-61] 0 -> 4\n"
+    "4. byte [61-61] 0 -> 5\n"
+    "5. byte [61-61] 0 -> 6\n"
+    "6. byte [61-61] 0 -> 7\n"
     "7. match! 0\n" },
   { "(a)",
     "3. capture 2 -> 4\n"
-    "4. byte [61-61] -> 5\n"
+    "4. byte [61-61] 0 -> 5\n"
     "5. capture 3 -> 6\n"
     "6. match! 0\n" },
   { "(?:a)",
-    "3. byte [61-61] -> 4\n"
+    "3. byte [61-61] 0 -> 4\n"
     "4. match! 0\n" },
   { "",
     "3. match! 0\n" },
   { ".",
-    "3+ byte [00-09] -> 5\n"
-    "4. byte [0b-ff] -> 5\n"
+    "3+ byte [00-09] 0 -> 5\n"
+    "4. byte [0b-ff] 0 -> 5\n"
     "5. match! 0\n" },
   { "[^ab]",
-    "3+ byte [00-09] -> 6\n"
-    "4+ byte [0b-60] -> 6\n"
-    "5. byte [63-ff] -> 6\n"
+    "3+ byte [00-09] 0 -> 6\n"
+    "4+ byte [0b-60] 0 -> 6\n"
+    "5. byte [63-ff] 0 -> 6\n"
     "6. match! 0\n" },
   { "[Aa]",
-    "3. byte/i [61-61] -> 4\n"
+    "3. byte/i [61-61] 0 -> 4\n"
     "4. match! 0\n" },
   { "\\C+",
-    "3. byte [00-ff] -> 4\n"
+    "3. byte [00-ff] 0 -> 4\n"
     "4+ altmatch -> 5 | 6\n"
     "5+ nop -> 3\n"
     "6. match! 0\n" },
   { "\\C*",
     "3+ altmatch -> 4 | 5\n"
-    "4+ byte [00-ff] -> 3\n"
+    "4+ byte [00-ff] 1 -> 3\n"
     "5. match! 0\n" },
   { "\\C?",
-    "3+ byte [00-ff] -> 5\n"
+    "3+ byte [00-ff] 1 -> 5\n"
     "4. nop -> 5\n"
     "5. match! 0\n" },
   // Issue 20992936
   { "[[-`]",
-    "3. byte [5b-60] -> 4\n"
+    "3. byte [5b-60] 0 -> 4\n"
     "4. match! 0\n" },
 };
 
@@ -268,51 +268,51 @@ TEST(TestCompile, Bug26705922) {
   string forward, reverse;
 
   Dump("[\\x{10000}\\x{10010}]", Regexp::LikePerl, &forward, &reverse);
-  EXPECT_EQ("3. byte [f0-f0] -> 4\n"
-            "4. byte [90-90] -> 5\n"
-            "5. byte [80-80] -> 6\n"
-            "6+ byte [80-80] -> 8\n"
-            "7. byte [90-90] -> 8\n"
+  EXPECT_EQ("3. byte [f0-f0] 0 -> 4\n"
+            "4. byte [90-90] 0 -> 5\n"
+            "5. byte [80-80] 0 -> 6\n"
+            "6+ byte [80-80] 0 -> 8\n"
+            "7. byte [90-90] 0 -> 8\n"
             "8. match! 0\n",
             forward);
-  EXPECT_EQ("3+ byte [80-80] -> 5\n"
-            "4. byte [90-90] -> 5\n"
-            "5. byte [80-80] -> 6\n"
-            "6. byte [90-90] -> 7\n"
-            "7. byte [f0-f0] -> 8\n"
+  EXPECT_EQ("3+ byte [80-80] 0 -> 5\n"
+            "4. byte [90-90] 0 -> 5\n"
+            "5. byte [80-80] 0 -> 6\n"
+            "6. byte [90-90] 0 -> 7\n"
+            "7. byte [f0-f0] 0 -> 8\n"
             "8. match! 0\n",
             reverse);
 
   Dump("[\\x{8000}-\\x{10FFF}]", Regexp::LikePerl, &forward, &reverse);
-  EXPECT_EQ("3+ byte [e8-ef] -> 5\n"
-            "4. byte [f0-f0] -> 8\n"
-            "5. byte [80-bf] -> 6\n"
-            "6. byte [80-bf] -> 7\n"
+  EXPECT_EQ("3+ byte [e8-ef] 0 -> 5\n"
+            "4. byte [f0-f0] 0 -> 8\n"
+            "5. byte [80-bf] 0 -> 6\n"
+            "6. byte [80-bf] 0 -> 7\n"
             "7. match! 0\n"
-            "8. byte [90-90] -> 5\n",
+            "8. byte [90-90] 0 -> 5\n",
             forward);
-  EXPECT_EQ("3. byte [80-bf] -> 4\n"
-            "4. byte [80-bf] -> 5\n"
-            "5+ byte [e8-ef] -> 7\n"
-            "6. byte [90-90] -> 8\n"
+  EXPECT_EQ("3. byte [80-bf] 0 -> 4\n"
+            "4. byte [80-bf] 0 -> 5\n"
+            "5+ byte [e8-ef] 0 -> 7\n"
+            "6. byte [90-90] 0 -> 8\n"
             "7. match! 0\n"
-            "8. byte [f0-f0] -> 7\n",
+            "8. byte [f0-f0] 0 -> 7\n",
             reverse);
 
   Dump("[\\x{80}-\\x{10FFFF}]", Regexp::LikePerl, NULL, &reverse);
-  EXPECT_EQ("3. byte [80-bf] -> 4\n"
-            "4+ byte [c2-df] -> 7\n"
-            "5+ byte [a0-bf] -> 8\n"
-            "6. byte [80-bf] -> 9\n"
+  EXPECT_EQ("3. byte [80-bf] 0 -> 4\n"
+            "4+ byte [c2-df] 0 -> 7\n"
+            "5+ byte [a0-bf] 1 -> 8\n"
+            "6. byte [80-bf] 0 -> 9\n"
             "7. match! 0\n"
-            "8. byte [e0-e0] -> 7\n"
-            "9+ byte [e1-ef] -> 7\n"
-            "10+ byte [90-bf] -> 13\n"
-            "11+ byte [80-bf] -> 14\n"
-            "12. byte [80-8f] -> 15\n"
-            "13. byte [f0-f0] -> 7\n"
-            "14. byte [f1-f3] -> 7\n"
-            "15. byte [f4-f4] -> 7\n",
+            "8. byte [e0-e0] 0 -> 7\n"
+            "9+ byte [e1-ef] 0 -> 7\n"
+            "10+ byte [90-bf] 1 -> 13\n"
+            "11+ byte [80-bf] 1 -> 14\n"
+            "12. byte [80-8f] 0 -> 15\n"
+            "13. byte [f0-f0] 0 -> 7\n"
+            "14. byte [f1-f3] 0 -> 7\n"
+            "15. byte [f4-f4] 0 -> 7\n",
             reverse);
 }
 
@@ -323,11 +323,11 @@ TEST(TestCompile, Bug35237384) {
   string forward;
 
   Dump("a**{3,}", Regexp::Latin1|Regexp::NeverCapture, &forward, NULL);
-  EXPECT_EQ("3+ byte [61-61] -> 3\n"
+  EXPECT_EQ("3+ byte [61-61] 1 -> 3\n"
             "4. nop -> 5\n"
-            "5+ byte [61-61] -> 5\n"
+            "5+ byte [61-61] 1 -> 5\n"
             "6. nop -> 7\n"
-            "7+ byte [61-61] -> 7\n"
+            "7+ byte [61-61] 1 -> 7\n"
             "8. match! 0\n",
             forward);
 
@@ -335,17 +335,17 @@ TEST(TestCompile, Bug35237384) {
   EXPECT_EQ("3+ nop -> 6\n"
             "4+ nop -> 8\n"
             "5. nop -> 21\n"
-            "6+ byte [61-61] -> 6\n"
+            "6+ byte [61-61] 1 -> 6\n"
             "7. nop -> 3\n"
-            "8+ byte [62-62] -> 8\n"
+            "8+ byte [62-62] 1 -> 8\n"
             "9. nop -> 3\n"
-            "10+ byte [61-61] -> 10\n"
+            "10+ byte [61-61] 1 -> 10\n"
             "11. nop -> 21\n"
-            "12+ byte [62-62] -> 12\n"
+            "12+ byte [62-62] 1 -> 12\n"
             "13. nop -> 21\n"
-            "14+ byte [61-61] -> 14\n"
+            "14+ byte [61-61] 1 -> 14\n"
             "15. nop -> 18\n"
-            "16+ byte [62-62] -> 16\n"
+            "16+ byte [62-62] 1 -> 16\n"
             "17. nop -> 18\n"
             "18+ nop -> 14\n"
             "19+ nop -> 16\n"
@@ -359,38 +359,38 @@ TEST(TestCompile, Bug35237384) {
   EXPECT_EQ("3+ nop -> 36\n"
             "4+ nop -> 31\n"
             "5. nop -> 33\n"
-            "6+ byte [00-09] -> 8\n"
-            "7. byte [0b-ff] -> 8\n"
+            "6+ byte [00-09] 0 -> 8\n"
+            "7. byte [0b-ff] 0 -> 8\n"
             "8+ nop -> 6\n"
             "9+ nop -> 29\n"
             "10. nop -> 28\n"
-            "11+ byte [00-09] -> 13\n"
-            "12. byte [0b-ff] -> 13\n"
+            "11+ byte [00-09] 0 -> 13\n"
+            "12. byte [0b-ff] 0 -> 13\n"
             "13+ nop -> 11\n"
             "14+ nop -> 26\n"
             "15. nop -> 28\n"
-            "16+ byte [00-09] -> 18\n"
-            "17. byte [0b-ff] -> 18\n"
+            "16+ byte [00-09] 0 -> 18\n"
+            "17. byte [0b-ff] 0 -> 18\n"
             "18+ nop -> 16\n"
             "19+ nop -> 36\n"
             "20. nop -> 33\n"
-            "21+ byte [00-09] -> 23\n"
-            "22. byte [0b-ff] -> 23\n"
+            "21+ byte [00-09] 0 -> 23\n"
+            "22. byte [0b-ff] 0 -> 23\n"
             "23+ nop -> 21\n"
             "24+ nop -> 31\n"
             "25. nop -> 33\n"
             "26+ nop -> 28\n"
-            "27. byte [53-53] -> 11\n"
+            "27. byte [53-53] 0 -> 11\n"
             "28. match! 0\n"
             "29+ nop -> 28\n"
-            "30. byte [53-53] -> 6\n"
+            "30. byte [53-53] 0 -> 6\n"
             "31+ nop -> 33\n"
-            "32. byte [53-53] -> 21\n"
+            "32. byte [53-53] 0 -> 21\n"
             "33+ nop -> 29\n"
             "34+ nop -> 26\n"
             "35. nop -> 28\n"
             "36+ nop -> 33\n"
-            "37. byte [53-53] -> 16\n",
+            "37. byte [53-53] 0 -> 16\n",
       forward);
 }
 
