f1980488142ee380b1dc54aea166ebdb5409b5ae
Paul Wankadia <junyer@google.com>
2019-03-18 00:31:06 -0700

    Get rid of `using std::string;`. (part 1 of N)
    Change-Id: I315e5e6bf1281ec252e68ab4e50357852e75e0ea
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/39110
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit bc5d6ae59f2bf8fa887e1fa6b00e8662d5953d86)

--- re2/filtered_re2.h
+++ re2/filtered_re2.h
@@ -49,7 +49,7 @@ class FilteredRE2 {
   // the search text should be lowercased first to find matching
   // strings from the set of strings returned by Compile.  Call after
   // all Add calls are done.
-  void Compile(std::vector<string>* strings_to_match);
+  void Compile(std::vector<std::string>* strings_to_match);
 
   // Returns the index of the first matching regexp.
   // Returns -1 on no match. Can be called prior to Compile.
