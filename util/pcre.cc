a3412b9c48eb7ca9d07fca5517fc1f9cdce2c9f9
Paul Wankadia <junyer@google.com>
2019-07-26 01:10:30 -0700

    Get rid of SStringPrintf().
    Change-Id: I33eed8a36587cc94c9b196e951872ff1e5dcaaa4
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/43070
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit 2f97859562d22be18309611fa077839104a63081)
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/43071

--- util/pcre.cc
+++ util/pcre.cc
@@ -666,9 +666,10 @@ bool PCRE::CheckRewriteString(absl::string_view rewrite,
   }
 
   if (max_token > NumberOfCapturingGroups()) {
-    SStringPrintf(error, "Rewrite schema requests %d matches, "
-                  "but the regexp only has %d parenthesized subexpressions.",
-                  max_token, NumberOfCapturingGroups());
+    *error = StringPrintf(
+        "Rewrite schema requests %d matches, but the regexp only has %d "
+        "parenthesized subexpressions.",
+        max_token, NumberOfCapturingGroups());
     return false;
   }
   return true;
