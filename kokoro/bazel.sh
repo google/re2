bc3ee2ab753309cbf4e65e0e8e00bd859a613577
Paul Wankadia <junyer@google.com>
2019-07-31 02:20:09 -0700

    Switch from //... to //:all when building with Bazel.
    Change-Id: I7db5fec22d6942964e8ecd3bc6987c1b0190a206
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/43250
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit 67bce690decdb507b13e14050661f8b9ebfcfe6c)
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/43251

--- kokoro/bazel.sh
+++ kokoro/bazel.sh
@@ -4,8 +4,8 @@ set -eux
 cd git/re2
 
 bazel clean
-bazel build --compilation_mode=dbg -- //...
-bazel test  --compilation_mode=dbg --test_output=errors -- //... \
+bazel build --compilation_mode=dbg -- //:all
+bazel test  --compilation_mode=dbg --test_output=errors -- //:all \
   -//:dfa_test \
   -//:exhaustive1_test \
   -//:exhaustive2_test \
@@ -14,8 +14,8 @@ bazel test  --compilation_mode=dbg --test_output=errors -- //... \
   -//:random_test
 
 bazel clean
-bazel build --compilation_mode=opt -- //...
-bazel test  --compilation_mode=opt --test_output=errors -- //... \
+bazel build --compilation_mode=opt -- //:all
+bazel test  --compilation_mode=opt --test_output=errors -- //:all \
   -//:dfa_test \
   -//:exhaustive1_test \
   -//:exhaustive2_test \
