1eadecd665803ab9d9e91cb5360c9bb93631e314
Paul Wankadia <junyer@google.com>
2019-10-09 01:36:12 -0700

    Tidy up the ersatz benchmark library.
    Change-Id: I3302c44127ab681421c14dd79afd9b85958e7467
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/46650
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit 266119da0a8328ecb18fbd31398100d2ec230619)

--- util/benchmark.cc
+++ util/benchmark.cc
@@ -7,10 +7,14 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <chrono>
-#include <thread>
 
+<<<<<<< HEAD   (604418 Move pod_array.h and sparse_{array,set}.h from util/ to re2/)
 #include "util/flags.h"
+=======
+>>>>>>> CHANGE (266119 Tidy up the ersatz benchmark library.)
 #include "util/benchmark.h"
+#include "util/flags.h"
+#include "util/util.h"
 #include "re2/re2.h"
 
 DEFINE_string(test_tmpdir, "/var/tmp", "temp directory");
@@ -19,142 +23,125 @@ DEFINE_string(test_tmpdir, "/var/tmp", "temp directory");
 #define snprintf _snprintf
 #endif
 
-using testing::Benchmark;
+using ::testing::Benchmark;
 
 static Benchmark* benchmarks[10000];
 static int nbenchmarks;
 
 void Benchmark::Register() {
-	benchmarks[nbenchmarks] = this;
-	if(lo < 1)
-		lo = 1;
-	if(hi < lo)
-		hi = lo;
-	nbenchmarks++;
+  lo_ = std::max(1, lo_);
+  hi_ = std::max(lo_, hi_);
+  benchmarks[nbenchmarks++] = this;
 }
 
 static int64_t nsec() {
-	return std::chrono::duration_cast<std::chrono::nanoseconds>(
-		std::chrono::steady_clock::now().time_since_epoch()).count();
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(
+             std::chrono::steady_clock::now().time_since_epoch())
+      .count();
 }
 
-static int64_t bytes;
-static int64_t ns;
 static int64_t t0;
+static int64_t ns;
+static int64_t bytes;
 static int64_t items;
 
-void SetBenchmarkBytesProcessed(int64_t x) {
-	bytes = x;
-}
-
-void StopBenchmarkTiming() {
-	if(t0 != 0)
-		ns += nsec() - t0;
-	t0 = 0;
+void BenchmarkMemoryUsage() {
+  // Pretend to support benchmarking memory usage.
 }
 
 void StartBenchmarkTiming() {
-	if(t0 == 0)
-		t0 = nsec();
+  if (t0 == 0) {
+    t0 = nsec();
+  }
 }
 
-void SetBenchmarkItemsProcessed(int n) {
-	items = n;
+void StopBenchmarkTiming() {
+  if (t0 != 0) {
+    ns += nsec() - t0;
+    t0 = 0;
+  }
 }
 
-void BenchmarkMemoryUsage() {
-	// TODO(rsc): Implement.
-}
+void SetBenchmarkBytesProcessed(int64_t b) { bytes = b; }
+
+void SetBenchmarkItemsProcessed(int i) { items = i; }
 
 int NumCPUs() {
-	return static_cast<int>(std::thread::hardware_concurrency());
+  // Pretend to support multi-threaded benchmarking.
+  return static_cast<int>(std::thread::hardware_concurrency());
 }
 
-static void runN(Benchmark *b, int n, int siz) {
-	bytes = 0;
-	items = 0;
-	ns = 0;
-	t0 = nsec();
-	if(b->fn)
-		b->fn(n);
-	else if(b->fnr)
-		b->fnr(n, siz);
-	else {
-		fprintf(stderr, "%s: missing function\n", b->name);
-		abort();
-	}
-	if(t0 != 0)
-		ns += nsec() - t0;
+static void RunFunc(Benchmark* b, int iters, int arg) {
+  t0 = nsec();
+  ns = 0;
+  bytes = 0;
+  items = 0;
+  b->func()(iters, arg);
+  StopBenchmarkTiming();
 }
 
 static int round(int n) {
-	int base = 1;
-	
-	while(base*10 < n)
-		base *= 10;
-	if(n < 2*base)
-		return 2*base;
-	if(n < 5*base)
-		return 5*base;
-	return 10*base;
+  int base = 1;
+  while (base * 10 < n) base *= 10;
+  if (n < 2 * base) return 2 * base;
+  if (n < 5 * base) return 5 * base;
+  return 10 * base;
 }
 
-void RunBench(Benchmark* b, int nthread, int siz) {
-	int n, last;
+static void RunBench(Benchmark* b, int arg) {
+  int iters, last;
 
-	// TODO(rsc): Threaded benchmarks.
-	if(nthread != 1)
-		return;
-	
-	// run once in case it's expensive
-	n = 1;
-	runN(b, n, siz);
-	while(ns < (int)1e9 && n < (int)1e9) {
-		last = n;
-		if(ns/n == 0)
-			n = (int)1e9;
-		else
-			n = (int)1e9 / static_cast<int>(ns/n);
-		
-		n = std::max(last+1, std::min(n+n/2, 100*last));
-		n = round(n);
-		runN(b, n, siz);
-	}
-	
-	char mb[100];
-	char suf[100];
-	mb[0] = '\0';
-	suf[0] = '\0';
-	if(ns > 0 && bytes > 0)
-		snprintf(mb, sizeof mb, "\t%7.2f MB/s", ((double)bytes/1e6)/((double)ns/1e9));
-	if(b->fnr || b->lo != b->hi) {
-		if(siz >= (1<<20))
-			snprintf(suf, sizeof suf, "/%dM", siz/(1<<20));
-		else if(siz >= (1<<10))
-			snprintf(suf, sizeof suf, "/%dK", siz/(1<<10));
-		else
-			snprintf(suf, sizeof suf, "/%d", siz);
-	}
-	printf("%s%s\t%8lld\t%10lld ns/op%s\n", b->name, suf, (long long)n, (long long)ns/n, mb);
-	fflush(stdout);
+  // Run once just in case it's expensive.
+  iters = 1;
+  RunFunc(b, iters, arg);
+  while (ns < (int)1e9 && iters < (int)1e9) {
+    last = iters;
+    if (ns / iters == 0) {
+      iters = (int)1e9;
+    } else {
+      iters = (int)1e9 / static_cast<int>(ns / iters);
+    }
+    iters = std::max(last + 1, std::min(iters + iters / 2, 100 * last));
+    iters = round(iters);
+    RunFunc(b, iters, arg);
+  }
+
+  char mb[100];
+  char suf[100];
+  mb[0] = '\0';
+  suf[0] = '\0';
+  if (ns > 0 && bytes > 0)
+    snprintf(mb, sizeof mb, "\t%7.2f MB/s",
+             ((double)bytes / 1e6) / ((double)ns / 1e9));
+  if (b->arg()) {
+    if (arg >= (1 << 20)) {
+      snprintf(suf, sizeof suf, "/%dM", arg / (1 << 20));
+    } else if (arg >= (1 << 10)) {
+      snprintf(suf, sizeof suf, "/%dK", arg / (1 << 10));
+    } else {
+      snprintf(suf, sizeof suf, "/%d", arg);
+    }
+  }
+  printf("%s%s\t%8d\t%10lld ns/op%s\n", b->name(), suf, iters,
+         (long long)ns / iters, mb);
+  fflush(stdout);
 }
 
-static int match(const char* name, int argc, const char** argv) {
-	if(argc == 1)
-		return 1;
-	for(int i = 1; i < argc; i++)
-		if(RE2::PartialMatch(name, argv[i]))
-			return 1;
-	return 0;
+static bool WantBench(const char* name, int argc, const char** argv) {
+  if (argc == 1) return true;
+  for (int i = 1; i < argc; i++) {
+    if (RE2::PartialMatch(name, argv[i]))
+      return true;
+  }
+  return false;
 }
 
 int main(int argc, const char** argv) {
-	for(int i = 0; i < nbenchmarks; i++) {
-		Benchmark* b = benchmarks[i];
-		if(match(b->name, argc, argv))
-			for(int j = b->threadlo; j <= b->threadhi; j++)
-				for(int k = std::max(b->lo, 1); k <= std::max(b->hi, 1); k<<=1)
-					RunBench(b, j, k);
-	}
+  for (int i = 0; i < nbenchmarks; i++) {
+    Benchmark* b = benchmarks[i];
+    if (!WantBench(b->name(), argc, argv))
+      continue;
+    for (int arg = b->lo(); arg <= b->hi(); arg <<= 1)
+      RunBench(b, arg);
+  }
 }
-
