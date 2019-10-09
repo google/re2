8d615ac724b97511ff888f51570aca481a037930
Paul Wankadia <junyer@google.com>
2019-10-09 01:36:12 -0700

    Tidy up the ersatz benchmark library.
    Change-Id: I3302c44127ab681421c14dd79afd9b85958e7467
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/46650
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit 266119da0a8328ecb18fbd31398100d2ec230619)

--- util/benchmark.h
+++ util/benchmark.h
@@ -6,38 +6,71 @@
 #define UTIL_BENCHMARK_H_
 
 #include <stdint.h>
+#include <functional>
+#include <thread>
 
 namespace testing {
-struct Benchmark {
-  const char* name;
-  void (*fn)(int);
-  void (*fnr)(int, int);
-  int lo;
-  int hi;
-  int threadlo;
-  int threadhi;
 
+class Benchmark {
+ public:
+  Benchmark(const char* name, void (*func)(int))
+      : name_(name),
+        func_([func](int iters, int arg) { func(iters); }),
+        lo_(0),
+        hi_(0),
+        arg_(false) {
+    Register();
+  }
+
+  Benchmark(const char* name, void (*func)(int, int), int lo, int hi)
+      : name_(name),
+        func_([func](int iters, int arg) { func(iters, arg); }),
+        lo_(lo),
+        hi_(hi),
+        arg_(true) {
+    Register();
+  }
+
+  Benchmark* ThreadRange(int lo, int hi) {
+    // Pretend to support multi-threaded benchmarking.
+    return this;
+  }
+
+  const char* name() const { return name_; }
+  const std::function<void(int, int)>& func() const { return func_; }
+  int lo() const { return lo_; }
+  int hi() const { return hi_; }
+  bool arg() const { return arg_; }
+
+ private:
   void Register();
-  Benchmark(const char* name, void (*f)(int)) { Clear(name); fn = f; Register(); }
-  Benchmark(const char* name, void (*f)(int, int), int l, int h) { Clear(name); fnr = f; lo = l; hi = h; Register(); }
-  void Clear(const char* n) { name = n; fn = 0; fnr = 0; lo = 0; hi = 0; threadlo = 0; threadhi = 0; }
-  Benchmark* ThreadRange(int lo, int hi) { threadlo = lo; threadhi = hi; return this; }
+
+  const char* name_;
+  std::function<void(int, int)> func_;
+  int lo_;
+  int hi_;
+  bool arg_;
+
+  Benchmark(const Benchmark&) = delete;
+  Benchmark& operator=(const Benchmark&) = delete;
 };
+
 }  // namespace testing
 
-void SetBenchmarkBytesProcessed(int64_t);
-void StopBenchmarkTiming();
-void StartBenchmarkTiming();
 void BenchmarkMemoryUsage();
-void SetBenchmarkItemsProcessed(int);
+void StartBenchmarkTiming();
+void StopBenchmarkTiming();
+void SetBenchmarkBytesProcessed(int64_t b);
+void SetBenchmarkItemsProcessed(int i);
 
 int NumCPUs();
 
-#define BENCHMARK(f) \
-	::testing::Benchmark* _benchmark_##f = (new ::testing::Benchmark(#f, f))
+#define BENCHMARK(f)                     \
+  ::testing::Benchmark* _benchmark_##f = \
+      (new ::testing::Benchmark(#f, f))
 
-#define BENCHMARK_RANGE(f, lo, hi) \
-	::testing::Benchmark* _benchmark_##f = \
-	(new ::testing::Benchmark(#f, f, lo, hi))
+#define BENCHMARK_RANGE(f, lo, hi)       \
+  ::testing::Benchmark* _benchmark_##f = \
+      (new ::testing::Benchmark(#f, f, lo, hi))
 
 #endif  // UTIL_BENCHMARK_H_
