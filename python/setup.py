860da5cba12706266765da5833d04cdbcf9890cd
Paul Wankadia <junyer@google.com>
2019-12-09 07:55:08 -0800

    Make the Python bindings support buffers.
    Change-Id: Ice0534aaff58466b32fbf9cfe4b089f68ff67b88

--- python/setup.py
+++ python/setup.py
@@ -36,7 +36,7 @@ ext_module = setuptools.Extension(
 
 setuptools.setup(
     name='google-re2',
-    version='0.0.3',
+    version='0.0.4',
     description='RE2 Python bindings',
     long_description=long_description,
     long_description_content_type='text/plain',
