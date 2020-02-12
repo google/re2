42d10accbfd50f1c1ba895c04b8beb4d8b9d8bbd
Paul Wankadia <junyer@google.com>
2020-02-11 18:19:49 -0800

    Tidy up some GCC-only guards.
    Change-Id: I3890ba72c9ec81f7f9e211d82d5cd3f87e0095b3
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/51830
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- util/pcre.cc
+++ util/pcre.cc
@@ -22,9 +22,7 @@
 #include "util/strutil.h"
 
 // Silence warnings about the wacky formatting in the operator() functions.
-// Note that we test for Clang first because it defines __GNUC__ as well.
-#if defined(__clang__)
-#elif defined(__GNUC__) && __GNUC__ >= 6
+#if !defined(__clang__) && defined(__GNUC__) && __GNUC__ >= 6
 #pragma GCC diagnostic ignored "-Wmisleading-indentation"
 #endif
 
