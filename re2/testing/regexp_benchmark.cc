4dfed5ffd3b405aefa816f829f2103aaf787802c
Paul Wankadia <junyer@google.com>
2020-02-17 23:51:30 -0800

    Move DeBruijnString() alongside StringGenerator.
    Change-Id: Id7e2ed4be60611c0a4e56c621d9449a595c94c5e

--- re2/testing/regexp_benchmark.cc
+++ re2/testing/regexp_benchmark.cc
@@ -792,40 +792,6 @@ BENCHMARK_RANGE(SearchPhone_CachedRE2, 8, 16<<20)->ThreadRange(1, NumCPUs());
 
 /*
 TODO(rsc): Make this work again.
-
-// Generates and returns a string over binary alphabet {0,1} that contains
-// all possible binary sequences of length n as subsequences.  The obvious
-// brute force method would generate a string of length n * 2^n, but this
-// generates a string of length n + 2^n - 1 called a De Bruijn cycle.
-// See Knuth, The Art of Computer Programming, Vol 2, Exercise 3.2.2 #17.
-static std::string DeBruijnString(int n) {
-  CHECK_LT(n, 8*sizeof(int));
-  CHECK_GT(n, 0);
-
-  std::vector<bool> did(1<<n);
-  for (int i = 0; i < 1<<n; i++)
-    did[i] = false;
-
-  std::string s;
-  for (int i = 0; i < n-1; i++)
-    s.append("0");
-  int bits = 0;
-  int mask = (1<<n) - 1;
-  for (int i = 0; i < (1<<n); i++) {
-    bits <<= 1;
-    bits &= mask;
-    if (!did[bits|1]) {
-      bits |= 1;
-      s.append("1");
-    } else {
-      s.append("0");
-    }
-    CHECK(!did[bits]);
-    did[bits] = true;
-  }
-  return s;
-}
-
 void CacheFill(int iters, int n, SearchImpl *srch) {
   std::string s = DeBruijnString(n+1);
   std::string t;
