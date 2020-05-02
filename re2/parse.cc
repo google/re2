be7fea6c11628a32b52ca0eff40a65e24ba05def
Paul Wankadia <junyer@google.com>
2020-05-02 01:50:23 -0700

    `^' is a caret, not a carat.
    Change-Id: I644c0d8f9e0d46fd8e652fa06e103d9830fc1bd3
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/55710
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit 8aef3d19dba9feae76881c30fb8e88c420c140c5)
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/55711

--- re2/parse.cc
+++ re2/parse.cc
@@ -91,7 +91,7 @@ class Regexp::ParseState {
   bool PushSimpleOp(RegexpOp op);
 
   // Pushes a ^ onto the stack.
-  bool PushCarat();
+  bool PushCaret();
 
   // Pushes a \b (word == true) or \B (word == false) onto the stack.
   bool PushWordBoundary(bool word);
@@ -420,7 +420,7 @@ bool Regexp::ParseState::PushLiteral(Rune r) {
 }
 
 // Pushes a ^ onto the stack.
-bool Regexp::ParseState::PushCarat() {
+bool Regexp::ParseState::PushCaret() {
   if (flags_ & OneLine) {
     return PushSimpleOp(kRegexpBeginText);
   }
@@ -2269,7 +2269,7 @@ Regexp* Regexp::Parse(absl::string_view s, ParseFlags global_flags,
         break;
 
       case '^':  // Beginning of line.
-        if (!ps.PushCarat())
+        if (!ps.PushCaret())
           return NULL;
         t.remove_prefix(1);  // '^'
         break;
