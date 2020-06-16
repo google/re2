23f748c0a739768cbca1c82a0682553c7d7a99a7
Paul Wankadia <junyer@google.com>
2020-06-16 07:36:33 -0700

    Make RE2::Set and FilteredRE2 movable.
    Change-Id: I2f5d431f59fc4ba015ada3a19ba40e7e4f4df2d3
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/57410
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- re2/filtered_re2.cc
+++ re2/filtered_re2.cc
@@ -27,8 +27,10 @@ FilteredRE2::FilteredRE2(int min_atom_len)
 FilteredRE2::~FilteredRE2() {
   for (size_t i = 0; i < re2_vec_.size(); i++)
     delete re2_vec_[i];
-  delete prefilter_tree_;
 }
+
+FilteredRE2::FilteredRE2(FilteredRE2&&) = default;
+FilteredRE2& FilteredRE2::operator=(FilteredRE2&&) = default;
 
 RE2::ErrorCode FilteredRE2::Add(const StringPiece& pattern,
                                 const RE2::Options& options, int* id) {
@@ -38,7 +40,7 @@ RE2::ErrorCode FilteredRE2::Add(const StringPiece& pattern,
   if (!re->ok()) {
     if (options.log_errors()) {
       LOG(ERROR) << "Couldn't compile regular expression, skipping: "
-                 << re << " due to error " << re->error();
+                 << pattern << " due to error " << re->error();
     }
     delete re;
   } else {
