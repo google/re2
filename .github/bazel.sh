c4ab9a6d012a35294cdd1d9209000a3dda0a7159
Paul Wankadia <junyer@google.com>
2020-09-22 10:40:58 -0700

    Migrate from Kokoro to GitHub Actions for Bazel.
    Change-Id: I4039cabd3bf9374718ae74f42e900ba99066085d
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/57795
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit 2be1c17fda0d6b0c8e3758b07d70ebe0af5eb2d2)
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/57796

--- .github/bazel.sh
+++ .github/bazel.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -eux
 
-cd git/re2
-
-case "${KOKORO_JOB_NAME}" in
-  */windows-*)
-    choco upgrade bazel -y -i
-    # Pin to Visual Studio 2015, which is the minimum that we support.
-    export BAZEL_VC='C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC'
-    ;;
-  *)
-    # Use the script provided by Kokoro.
-    use_bazel.sh latest
-    ;;
-esac
-
 bazel clean
 bazel build --compilation_mode=dbg -- //:all
 bazel test  --compilation_mode=dbg --test_output=errors -- //:all \
