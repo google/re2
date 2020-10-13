56fc28f7516a985dd13582cad3f6d1cd4be3620f
Paul Wankadia <junyer@google.com>
2020-10-13 02:20:27 -0700

    Don't support ParseFrom() on MSVC. It can cause ICEs.
    Change-Id: I712554d582c672924956ab813a9decff2fbcc8ab
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/57990
    Reviewed-by: Paul Wankadia <junyer@google.com>
    (cherry picked from commit 703e42edf95194e3d13dbaff3de154bb620d1d54)

--- re2/testing/re2_arg_test.cc
+++ re2/testing/re2_arg_test.cc
@@ -135,6 +135,7 @@ TEST(RE2ArgTest, Uint64Test) {
 }
 
 TEST(RE2ArgTest, ParseFromTest) {
+#if !defined(__clang__) && defined(_MSC_VER)
   struct {
     bool ParseFrom(const char* str, size_t n) {
       LOG(INFO) << "str = " << str << ", n = " << n;
@@ -154,6 +155,7 @@ TEST(RE2ArgTest, ParseFromTest) {
   } obj2;
   RE2::Arg arg2(&obj2);
   EXPECT_FALSE(arg2.Parse("two", 3));
+#endif
 }
 
 }  // namespace re2
