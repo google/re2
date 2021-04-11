8b0df4be74b1e25f21d60995c8db81cb3f5522e6
Paul Wankadia <junyer@google.com>
2021-04-11 07:32:39 -0700

    Upload source and wheels to PyPI.
    Change-Id: I82b7f5c374e20bde641648293dc59e2a8332afbb

--- python/pypi.sh
+++ python/pypi.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+# Uploads source and wheels to PyPI.
+# Expects to be used in a container:
+#   docker run -i -t --rm -v $PWD:/src quay.io/pypa/manylinux2014_x86_64
 set -eux
 
 SRCDIR=$(readlink --canonicalize $(dirname $0))
 DSTDIR=$(mktemp --directory --tmpdir $(basename $0).XXXXXXXXXX)
+
+cd ${DSTDIR}
+git clone --depth 1 https://github.com/google/re2.git
+cd re2
+perl -i -lpe 'if (/^CXXFLAGS/) { $_ .= " -fPIC"; }' Makefile
+make -j$(nproc) static-install
+cd ..
+rm -rf re2
 
 cd ${SRCDIR}
 cp _re2.cc re2.py setup.py ${DSTDIR}
@@ -14,8 +25,17 @@ sed -i -E \
   -e 's/absl::string_view/re2::StringPiece/' \
   -e 's/absl::make_unique<([^>]+)>\(([^)]+)\)/std::unique_ptr<\1>(new \1(\2))/' \
   _re2.cc
-python3 setup.py sdist
-twine upload dist/*
+for TAGS in cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39; do
+  PYTHON3=/opt/python/${TAGS}/bin/python3
+  ${PYTHON3} -m pip install pybind11
+  ${PYTHON3} setup.py bdist_wheel
+  ${PYTHON3} -m pip install auditwheel
+  ${PYTHON3} -m auditwheel repair --strip dist/*
+  rm dist/*
+done
+${PYTHON3} setup.py sdist
+${PYTHON3} -m pip install twine
+${PYTHON3} -m twine upload dist/* wheelhouse/*
 
 cd ${SRCDIR}
 rm -rf ${DSTDIR}
