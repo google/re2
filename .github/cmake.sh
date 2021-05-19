c47b5818a7cb7e282dadb1fc56f0d993a113294b
Paul Wankadia <junyer@google.com>
2021-05-19 07:32:29 -0700

    Pass a path to `cmake` in order to avoid a warning.
    Change-Id: I998b80494102a67ce3e23d8b09deb15874a85fb7
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/58612
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- .github/cmake.sh
+++ .github/cmake.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eux
 
-cmake -D CMAKE_BUILD_TYPE=Debug
+cmake . -D CMAKE_BUILD_TYPE=Debug
 cmake --build . --config Debug --clean-first
 ctest -C Debug --output-on-failure -E 'dfa|exhaustive|random'
 
-cmake -D CMAKE_BUILD_TYPE=Release
+cmake . -D CMAKE_BUILD_TYPE=Release
 cmake --build . --config Release --clean-first
 ctest -C Release --output-on-failure -E 'dfa|exhaustive|random'
 
