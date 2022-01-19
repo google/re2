6fbedc9722a49e545d86944db92463ada675c682
Paul Wankadia <junyer@google.com>
2022-01-18 20:12:43 -0800

    Address a `-Wunused-but-set-variable' warning from Clang 13.x.
    Change-Id: Ib7c4efdd9eba4987c080d4e78a5bb42771c399e0

--- re2/prog.cc
+++ re2/prog.cc
@@ -611,10 +611,13 @@ void Prog::Flatten() {
     inst_count_[ip->opcode()]++;
   }
 
-  int total = 0;
+#if !defined(NDEBUG)
+  // Address a `-Wunused-but-set-variable' warning from Clang 13.x.
+  size_t total = 0;
   for (int i = 0; i < kNumInst; i++)
     total += inst_count_[i];
-  DCHECK_EQ(total, static_cast<int>(flat.size()));
+  CHECK_EQ(total, flat.size());
+#endif
 
   // Remap start_unanchored and start.
   if (start_unanchored() == 0) {
