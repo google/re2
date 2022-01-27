0d39decb2f0adcbcb943fd31943fcd20398ebe4e
Paul Wankadia <junyer@google.com>
2022-01-27 08:38:08 -0800

    Handle older macOS SDKs not defining TARGET_OS_OSX.
    Change-Id: I4a0566f3f1cb2d6320e3de80619aa90e0f53ff27
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/59690
    Reviewed-by: Perry Lorier <perryl@google.com>
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- re2/re2.h
+++ re2/re2.h
@@ -971,7 +971,7 @@ namespace hooks {
 // As per https://github.com/google/re2/issues/325, thread_local support in
 // MinGW seems to be buggy. (FWIW, Abseil folks also avoid it.)
 #define RE2_HAVE_THREAD_LOCAL
-#if (defined(__APPLE__) && !TARGET_OS_OSX) || defined(__MINGW32__)
+#if (defined(__APPLE__) && !(defined(TARGET_OS_OSX) && TARGET_OS_OSX)) || defined(__MINGW32__)
 #undef RE2_HAVE_THREAD_LOCAL
 #endif
 
