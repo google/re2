9da20386ba02b1078211d94e2d926876d3060d77
Paul Wankadia <junyer@google.com>
2022-01-31 05:07:59 -0800

    Bump version to 0.2.20220201 for PyPI.
    Change-Id: I375fca8efcbc49d0ca122e053caae601a66a1b01

--- python/setup.py
+++ python/setup.py
@@ -47,7 +47,7 @@ ext_module = setuptools.Extension(
 
 setuptools.setup(
     name='google-re2',
-    version='0.2.20211101',
+    version='0.2.20220201',
     description='RE2 Python bindings',
     long_description=long_description,
     long_description_content_type='text/plain',
