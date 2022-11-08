9d65debf47ccf68e51857009d14845f0354b852c
Paul Wankadia <junyer@google.com>
2022-11-08 09:50:17 -0800

    Build the app with Bazel.
    Change-Id: I56bb7f5782c81bf09597a0cd02786bfa90a1d16c
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/60770
    Reviewed-by: Alex Chernyakhovsky <achernya@google.com>
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- app/build.sh
+++ app/build.sh
@@ -4,26 +4,36 @@ set -eux
 SRCDIR=$(readlink --canonicalize $(dirname $0))
 DSTDIR=$(mktemp --directory --tmpdir $(basename $0).XXXXXXXXXX)
 
+BAZEL=/tmp/bazel
+BAZELISK_RELEASE=v1.15.0
+
 if [[ ${UID} -ne 0 ]]; then
   if [[ -d deploy ]]; then
     echo -e '\033[1;31m' "** The ${PWD}/deploy directory exists! Refusing to clobber it! **" '\033[0m'
     exit 1
   fi
   mkdir deploy
-  sudo docker run -i -t --pull always --rm -v ${SRCDIR}:/src -v ${PWD}:/dst emscripten/emsdk /src/$(basename $0)
+  sudo docker run -i -t --pull always --rm -v ${SRCDIR}/..:/src -v ${PWD}:/dst emscripten/emsdk /src/app/$(basename $0)
   ls -l deploy
 else
-  cd ${DSTDIR}
-  git clone --depth 1 --branch main https://github.com/google/re2.git
-  cd re2
-  perl -i -lpe 's/-pthread //;' Makefile
-  emmake make -j$(nproc) obj/libre2.a
-  em++ --bind -s MODULARIZE=1 -s EXPORT_ES6=1 -s EXPORT_NAME=loadModule -Wall -Wextra -Wno-unused-parameter -I. -O3 -g -DNDEBUG -Lobj -lre2 \
-    ${SRCDIR}/_re2.cc -o ../_re2.js
-  cd ..
-  rm -rf re2
+  wget -O ${BAZEL} https://github.com/bazelbuild/bazelisk/releases/download/${BAZELISK_RELEASE}/bazelisk-linux-amd64
+  chmod +x ${BAZEL}
 
   cd ${SRCDIR}
+  # Emscripten doesn't support `-fstack-protector`.
+  AR=emar CC=emcc \
+    ${BAZEL} build --compilation_mode=opt \
+    --config=linux --copt=-fno-stack-protector \
+    -- :all
+  # Bazel doesn't retain the `_re2.wasm` artifact;
+  # we have to redo the link command to obtain it.
+  pushd ..
+  emcc @bazel-bin/app/_re2.js-2.params
+  cd bazel-bin/app
+  cp _re2.js _re2.wasm ${DSTDIR}
+  popd
+  # Clean up the sundry Bazel output directories.
+  ${BAZEL} clean --expunge
   cp app.ts index.html _re2.d.ts ${DSTDIR}
   cp package.json rollup.config.js tsconfig.json ${DSTDIR}
 
