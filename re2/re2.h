891fa668468f201f6a0c16bbff76f26d8ae59e82
Paul Wankadia <junyer@google.com>
2023-02-10 09:30:03 -0800

    Fix a typographical error.
    Change-Id: Ie02f840baaa7c5056dfb281b2611c9d48525c51f
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/61010
    Reviewed-by: Perry Lorier <perryl@google.com>
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- re2/re2.h
+++ re2/re2.h
@@ -591,7 +591,7 @@ class RE2 {
   // Replace(). E.g. if rewrite == "foo \\2,\\1", returns 2.
   static int MaxSubmatch(const StringPiece& rewrite);
 
-  // Append the "rewrite" string, with backslash subsitutions from "vec",
+  // Append the "rewrite" string, with backslash substitutions from "vec",
   // to string "out".
   // Returns true on success.  This method can fail because of a malformed
   // rewrite string.  CheckRewriteString guarantees that the rewrite will
