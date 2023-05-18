3b66f66f0f51f88628176cf12a5c0b63fb636f03
Paul Wankadia <junyer@google.com>
2023-05-18 19:42:06 +0000

    For now, stop building the testing for RE2 with CMake.
    We must still know that building RE2 works, of course.
    
    Change-Id: Ibc39d880d5595cbfbed04b3ea73e8681090bdbf0
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/61293
    Reviewed-by: Alex Chernyakhovsky <achernya@google.com>
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- .github/cmake.sh
+++ .github/cmake.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -eux
 
-cmake_vars=(-D RE2_BUILD_TESTING=ON)
-if [[ ${RUNNER_OS} == Windows ]]; then
-  cmake_vars+=(-D CMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake)
-fi
-
-cmake . -D CMAKE_BUILD_TYPE=Debug "${cmake_vars[@]}"
+cmake . -D CMAKE_BUILD_TYPE=Debug "$@"
 cmake --build . --config Debug --clean-first
-ctest -C Debug --output-on-failure -E 'dfa|exhaustive|random'
+# TODO: Build the testing for RE2.
+# ctest -C Debug --output-on-failure -E 'dfa|exhaustive|random'
 
-cmake . -D CMAKE_BUILD_TYPE=Release "${cmake_vars[@]}"
+cmake . -D CMAKE_BUILD_TYPE=Release "$@"
 cmake --build . --config Release --clean-first
-ctest -C Release --output-on-failure -E 'dfa|exhaustive|random'
+# TODO: Build the testing for RE2.
+# ctest -C Release --output-on-failure -E 'dfa|exhaustive|random'
 
 exit 0
