15d23bf71bd6a0242aee7d38dc326666128d76f3
Paul Wankadia <junyer@google.com>
2023-05-19 08:43:08 +0000

    Try building the testing for RE2 with CMake again.
    Benchmark on Ubuntu seems problematic with LTO, so... disable LTO.
    
    GoogleTest on Windows makes MSVC's linker give "already defined in
    testing.lib(exhaustive_tester.obj)" errors, so... frob the testing
    library in the faint hope that the issue will finally be resolved.
    
    Change-Id: Ib4237d44a12aa5bd2783d6b1285a5ccb14df4665

--- .github/cmake.sh
+++ .github/cmake.sh
@@ -3,12 +3,10 @@ set -eux
 
 cmake . -D CMAKE_BUILD_TYPE=Debug "$@"
 cmake --build . --config Debug --clean-first
-# TODO: Build the testing for RE2.
-# ctest -C Debug --output-on-failure -E 'dfa|exhaustive|random'
+ctest -C Debug --output-on-failure -E 'dfa|exhaustive|random'
 
 cmake . -D CMAKE_BUILD_TYPE=Release "$@"
 cmake --build . --config Release --clean-first
-# TODO: Build the testing for RE2.
-# ctest -C Release --output-on-failure -E 'dfa|exhaustive|random'
+ctest -C Release --output-on-failure -E 'dfa|exhaustive|random'
 
 exit 0
