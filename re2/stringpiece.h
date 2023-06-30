2d39b703d02645076fead8fa409a1711f0e84381
Paul Wankadia <junyer@google.com>
2023-06-30 14:43:40 +0000

    Clean up some StringPiece-related cruft.
    Change-Id: I6bf7ccda2b0a80b782f3fb3fe05ba79f77082e22
    Reviewed-on: https://code-review.googlesource.com/c/re2/+/61490
    Reviewed-by: Alex Chernyakhovsky <achernya@google.com>
    Reviewed-by: Paul Wankadia <junyer@google.com>

--- re2/stringpiece.h
+++ re2/stringpiece.h
@@ -9,14 +9,8 @@
 
 namespace re2 {
 
-// RE2 has two versions: "sans Abseil" in the main branch; and "avec Abseil" in
-// the abseil branch. This has led to a diamond dependency problem for projects
-// like Envoy: as per https://github.com/google/re2/issues/388, GoogleTest took
-// a dependency on RE2 avec Abseil, but other things depend on RE2 sans Abseil.
-// To resolve this conflict until both versions can migrate to std::string_view
-// (C++17), those other things must be able to #include "re2/stringpiece.h" and
-// use re2::StringPiece. (This is a hack, obviously, but it beats telling every
-// project in this situation that they have to perform source transformations.)
+// Until RE2 requires C++17 and uses std::string_view, allow users to
+// continue to #include "re2/stringpiece.h" and use re2::StringPiece.
 using StringPiece = absl::string_view;
 
 }  // namespace re2
