b0c74f743c4fa468f18a1ad3fb098da0d0a2c5c7
Paul Wankadia <junyer@google.com>
2023-12-12 13:34:36 +0000

    Fix an old bug that can manifest during factoring.
    `AddFoldedRange()` can terminate prematurely if the character class
    already contains the rune. For example, if it contains `a` and we
    want to add folded `a`, it sees `a` and stops without adding `A`.
    To avoid that, we use an empty character class and then merge it.
    
    Fixes #467.
    
    Change-Id: I1f26b182e23b8a03da5d98107705bdf443d931d6

--- re2/testing/parse_test.cc
+++ re2/testing/parse_test.cc
@@ -356,6 +356,13 @@ Test prefix_tests[] = {
     "cat{lit{a}alt{emp{}cat{str{ardvark}alt{emp{}lit{s}}}"
     "cat{str{ba}alt{cat{lit{c}alt{cc{0x69 0x6b}cat{str{us}alt{emp{}str{es}}}}}"
     "str{ft}cat{str{lone}alt{emp{}lit{s}}}}}}}" },
+  // As per https://github.com/google/re2/issues/467,
+  // these should factor identically, but they didn't
+  // because AddFoldedRange() terminated prematurely.
+  { "0A|0[aA]", "cat{lit{0}cc{0x41 0x61}}" },
+  { "0a|0[aA]", "cat{lit{0}cc{0x41 0x61}}" },
+  { "0[aA]|0A", "cat{lit{0}cc{0x41 0x61}}" },
+  { "0[aA]|0a", "cat{lit{0}cc{0x41 0x61}}" },
 };
 
 // Test that prefix factoring works.
