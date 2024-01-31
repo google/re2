e09f55985de74e5e9e4197f2ca37c5ea84eedb14
Paul Wankadia <junyer@google.com>
2024-01-31 15:36:37 +0000

    Build and deploy to GitHub Pages from GitHub Actions.
    Change-Id: I4140492ee8ff367bbe65dd27f7dbd11f6fc7ad6f

--- app/build.sh
+++ app/build.sh
@@ -4,41 +4,29 @@ set -eux
 SRCDIR=$(readlink --canonicalize $(dirname $0))
 DSTDIR=$(mktemp --directory --tmpdir $(basename $0).XXXXXXXXXX)
 
-BAZEL=/tmp/bazel
-BAZELISK_RELEASE=v1.17.0
+cd ${SRCDIR}
+# Emscripten doesn't support `-fstack-protector`.
+AR=emar CC=emcc \
+  bazel build --compilation_mode=opt \
+  --copt=-fno-stack-protector \
+  -- :all
+cp ../bazel-bin/app/_re2.js ${DSTDIR}
+bazel clean --expunge
+cp app.ts index.html _re2.d.ts ${DSTDIR}
+cp package.json rollup.config.js tsconfig.json ${DSTDIR}
 
-if [[ ${UID} -ne 0 ]]; then
-  if [[ -d deploy ]]; then
-    echo -e '\033[1;31m' "** The ${PWD}/deploy directory exists! Refusing to clobber it! **" '\033[0m'
-    exit 1
-  fi
-  mkdir deploy
-  sudo docker run -i -t --pull always --rm -v ${SRCDIR}/..:/src -v ${PWD}:/dst emscripten/emsdk /src/app/$(basename $0)
-  ls -l deploy
-else
-  wget -O ${BAZEL} https://github.com/bazelbuild/bazelisk/releases/download/${BAZELISK_RELEASE}/bazelisk-linux-amd64
-  chmod +x ${BAZEL}
-
-  cd ${SRCDIR}
-  # Emscripten doesn't support `-fstack-protector`.
-  AR=emar CC=emcc \
-    ${BAZEL} build --compilation_mode=opt \
-    --copt=-fno-stack-protector \
-    -- :all
-  cp ../bazel-bin/app/_re2.js ${DSTDIR}
-  # Clean up the sundry Bazel output directories.
-  ${BAZEL} clean --expunge
-  cp app.ts index.html _re2.d.ts ${DSTDIR}
-  cp package.json rollup.config.js tsconfig.json ${DSTDIR}
-
-  cd ${DSTDIR}
-  npm install
-  npx tsc
-  npx rollup -c rollup.config.js -d deploy
-  mv deploy/* /dst/deploy
-fi
+cd ${DSTDIR}
+npm install
+npx tsc
+npx rollup -c rollup.config.js -d deploy
 
 cd ${SRCDIR}
-rm -rf ${DSTDIR}
+mkdir deploy
+cat >deploy/index.html <<EOF
+<html><head><meta http-equiv="refresh" content="0; url=https://github.com/google/re2"></head><body></body></html>
+EOF
+mkdir deploy/app
+cp ${DSTDIR}/deploy/* deploy/app
+ls -lR deploy
 
 exit 0
