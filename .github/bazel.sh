4fcd3a9daa150a156f0a32b432c091f3aa28b398
Paul Wankadia <junyer@google.com>
2024-03-02 18:10:21 +0000

    Update @pybind11_bazel to version 2.11.1.bzl.3.
    At long last, we can test the Python bindings in the CI
    because they should actually work on Windows as well...
    
    Change-Id: Iede307a2cc059fd3230d238e0861439971b4169f

--- .github/bazel.sh
+++ .github/bazel.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 set -eux
 
-bazel clean
-bazel build --compilation_mode=dbg -- //:all
-bazel test  --compilation_mode=dbg -- //:all \
-  -//:dfa_test \
-  -//:exhaustive1_test \
-  -//:exhaustive2_test \
-  -//:exhaustive3_test \
-  -//:exhaustive_test \
-  -//:random_test
-
-bazel clean
-bazel build --compilation_mode=opt -- //:all
-bazel test  --compilation_mode=opt -- //:all \
-  -//:dfa_test \
-  -//:exhaustive1_test \
-  -//:exhaustive2_test \
-  -//:exhaustive3_test \
-  -//:exhaustive_test \
-  -//:random_test
+for compilation_mode in dbg opt
+do
+  bazel clean
+  bazel build --compilation_mode=${compilation_mode} -- \
+    //:re2 \
+    //python:re2
+  bazel test  --compilation_mode=${compilation_mode} -- \
+    //:all \
+    -//:dfa_test \
+    -//:exhaustive1_test \
+    -//:exhaustive2_test \
+    -//:exhaustive3_test \
+    -//:exhaustive_test \
+    -//:random_test \
+    //python:all
+done
 
 exit 0
