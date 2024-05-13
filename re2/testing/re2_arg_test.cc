cb81784e52014cfb1189fae4c53294eeabbaae51
Paul Wankadia <junyer@google.com>
2024-05-13 18:23:14 +0000

    Restore `bool T::ParseFrom(const char*, size_t)` support for MSVC.
    IIRC, it was problematic for MSVC 2017 and/or MSVC 2019, but
    https://github.com/google/oss-policies-info/commit/dca3ce04cd812e03c71107a7fdea419acbd6432c
    raised the support baseline to MSVC 2022, so let's try it...
    
    Change-Id: Id00483381c1f6c499a2da0c2d40d235c95dedfaa

--- re2/testing/re2_arg_test.cc
+++ re2/testing/re2_arg_test.cc
@@ -135,7 +135,6 @@ TEST(RE2ArgTest, Uint64Test) {
 }
 
 TEST(RE2ArgTest, ParseFromTest) {
-#if !defined(_MSC_VER)
   struct {
     bool ParseFrom(const char* str, size_t n) {
       LOG(INFO) << "str = " << str << ", n = " << n;
@@ -155,7 +154,6 @@ TEST(RE2ArgTest, ParseFromTest) {
   } obj2;
   RE2::Arg arg2(&obj2);
   EXPECT_FALSE(arg2.Parse("two", 3));
-#endif
 }
 
 TEST(RE2ArgTest, OptionalDoubleTest) {
