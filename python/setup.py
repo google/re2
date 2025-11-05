6b6f716ebf2f23694ae08694f13bf618ee2f9cf6
Russ Cox <rsc@swtch.com>
2025-11-05 09:04:21 -0500

    re2: prepare to tag 2025-11-05

--- python/setup.py
+++ python/setup.py
@@ -129,7 +129,7 @@ try:
 
   setuptools.setup(
       name='google-re2',
-      version='1.1.20250812',
+      version='1.1.20251105',
       description='RE2 Python bindings',
       long_description=long_description,
       long_description_content_type='text/plain',
